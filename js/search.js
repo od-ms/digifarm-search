07da88948f2dfa2baa1d617e95df8db9d5be8fc0
Thomas Werner <thomas.w.werner@gmail.com>
2022-08-04 17:15:24 +0200

    New filter options for "Type" and "Help needed"

--- js/search.js
+++ js/search.js
@@ -31,21 +31,36 @@ function getProjectListHtml(projects = null) {
 
   let projectList = $('<ul class="list-group"></ul>');
   projects.forEach(function (project, key) {
-    let projectData = "<h3 class='fs-5'>" + project.Name + "</h3>";
+    let projectData = '<h3 class="fs-5">' + project.Name + "</h3>";
     if (project['Kurzbeschreibung']) {
       projectData += "<p>" + project['Kurzbeschreibung'] + "</p>";
     }
-    projectData += "<dl class='row'>";
-    const properties = ['Ursprung', 'Quelle', 'Kategorie', 'Komplexität', 'Technologien'];
+    projectData += '<dl class="row">';
+    const properties = ['Ursprung', 'Typ', 'Kategorie', 'Komplexität', 'Technologien'];
     properties.forEach(function (property) {
       if (project[property]) {
-        projectData += "<dt class='col-5 col-sm-4 col-md-3 col-xl-2 fw-normal'>" + property + ": " + "</dt>";
-        projectData += "<dd class='col-7 col-sm-8 col-md-9 col-xl-10 project-attribute'>" + project[property] + "</dd>"
+        projectData += '<dt class="col-5 col-sm-4 col-md-3 col-xl-2 fw-normal">'
+          + property + ": " + "</dt>";
+        projectData += '<dd class="col-7 col-sm-8 col-md-9 col-xl-10 project-attribute">'
+          + project[property] + "</dd>"
       }
     })
+    helpNeeded = '';
+    if (!project['Technik']) {
+      helpNeeded += '<span class="badge me-1 text-bg-secondary">Technik</span>'
+    }
+    if (!project['Inhalt']) {
+      helpNeeded += '<span class="badge me-1 text-bg-secondary">Inhalt</span>'
+    }
+    if (!project['Sponsor']) {
+      helpNeeded += '<span class="badge me-1 text-bg-secondary">Sponsor</span>'
+    }
+    projectData += '<dt class="col-5 col-sm-4 col-md-3 col-xl-2 fw-normal">Hilfe benötigt:</dt>'
+      +'<dd class="col-7 col-sm-8 col-md-9 col-xl-10 project-attribute">' + (helpNeeded || 'keine') + '</dd>';
     projectData += "</ul>";
     if (project['Projekt-Url']) {
-      projectData += "<p></p><a href='" + project['Projekt-Url'] + "' target=\"_blank\">Zum Projekt</a></p>";
+      projectData += '<p></p><a href="' + project['Digifarm-Url']
+        + '" target="_top">Zum Projekt</a></p>';
     }
     projectList.append("<li class='list-group-item' id='" + key + "'>" + projectData + "</li>");
   });
@@ -98,14 +113,22 @@ function getSelectOptionsHTMLByKey(key, projects = null) {
   })
   projectValues.sort();
 
+  return getSelectBoxHTML(key, projectValues);
+}
+
+function getSelectBoxHTML(key, projectValues) {
   let $options = $('<select class="filterbox form-select" name="' + key + '" id="' + key + '"></select>');
   $options.append('<option value="" selected>Bitte auswählen</option>');
   projectValues.forEach(function (projectValue) {
     $options.append('<option value="' + projectValue + '">' + projectValue + '</option>');
   });
-
-  return $options;
+  return $options
 }
+
+const OPTION_SHOW_ALL = 'alle Hilfsgesuche';
+const OPTION_SHOW_TECHNIK = 'Technik';
+const OPTION_SHOW_INHALT = 'Inhalt';
+const OPTION_SHOW_SPONSOR = 'Sponsoring';
 
 $().ready(function () {
 
@@ -115,14 +138,23 @@ $().ready(function () {
   $('.project-list').html(projectList);
 
   // Get the select box options for filter elements by key.
-  let originOptions = getSelectOptionsHTMLByKey('Ursprung');
-  $('#origin-select').html(originOptions);
+
+  let helpOptions = getSelectBoxHTML('Hilfe', [OPTION_SHOW_INHALT, OPTION_SHOW_TECHNIK, OPTION_SHOW_SPONSOR, OPTION_SHOW_ALL]);
+  $('#help-select').html(helpOptions);
+
+  // "origin" is currently unused
+  // let originOptions = getSelectOptionsHTMLByKey('Ursprung');
+  // $('#origin-select').html(originOptions);
 
   let technologyOptions = getSelectOptionsHTMLByKey('Technologien');
   $('#technology-select').html(technologyOptions);
 
-  let sourceOptions = getSelectOptionsHTMLByKey('Quelle');
-  $('#source-select').html(sourceOptions);
+  // "source" is currently unused
+  //let sourceOptions = getSelectOptionsHTMLByKey('Quelle');
+  //$('#source-select').html(sourceOptions);
+
+  let typeOptions = getSelectOptionsHTMLByKey('Typ');
+  $('#type-select').html(typeOptions);
 
   let categoryOptions = getSelectOptionsHTMLByKey('Kategorie');
   $('#category-select').html(categoryOptions);
@@ -137,11 +169,27 @@ $().ready(function () {
     var optionSelected = $("option:selected", this);
     var valueSelected = this.value;
 
-    if (valueSelected) {
+    console.debug("selected", this.name, optionSelected, valueSelected)
+
+    if ((this.name == "Hilfe") && valueSelected) {
+      if (valueSelected == OPTION_SHOW_ALL) {
+        filteredProjects = projects.filter(function(project) { return project["Inhalt"] == "" || project["Sponsor"] == "" || project["Technik"] == "" } );
+      } else if (valueSelected == OPTION_SHOW_TECHNIK) {
+        filteredProjects = projects.filter(function(project) { return project["Technik"].trim() == "" } );
+      } else if (valueSelected == OPTION_SHOW_SPONSOR) {
+        filteredProjects = projects.filter(function(project) { return project["Sponsor"].trim() == "" } );
+      } else if (valueSelected == OPTION_SHOW_INHALT) {
+        filteredProjects = projects.filter(function(project) { return project["Inhalt"].trim() == "" } );
+      } else {
+        filteredProjects = projects;
+      }
+    }
+    else if (valueSelected) {
       filteredProjects = filterData({value: this.value, key: this.name})
     } else {
       filteredProjects = projects;
     }
+    console.debug('filteredProjects', filteredProjects)
     projectHtml = getProjectListHtml(filteredProjects);
     $('.project-list').html(projectHtml);
   });
